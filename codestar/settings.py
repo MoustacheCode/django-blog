12921deb54a2a256b191748e09b979e654262069
Richard <r-duerden@live.co.uk>
2025-11-11 15:56:16 +0000

    security added

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lds+5ypg@^m9vs&_b4$w^x5465spuo^=gw3t(7hh7eo-829w%c'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
