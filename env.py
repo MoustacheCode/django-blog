7b25f917064052853eb6ae43047d2a10f19186a8
Richard <r-duerden@live.co.uk>
2025-11-11 15:37:05 +0000

    database added

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "<postgresql://neondb_owner:npg_kjpEYzAeT72m@ep-gentle-boat-agkpqa3s.c-2.eu-central-1.aws.neon.tech/trick_prank_kilt_122832>")
