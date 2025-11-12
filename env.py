10f8024e452f20e147c235d85fe8ca7efad282d1
Richard <r-duerden@live.co.uk>
2025-11-12 11:32:59 +0000

    added blog settings

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_kjpEYzAeT72m@ep-gentle-boat-agkpqa3s.c-2.eu-central-1.aws.neon.tech/trick_prank_kilt_122832")
+os.environ.setdefault("SECRET_KEY",
+                       "33398fd17cafb4f8b389f80018f5b61e0cfc74f62e14f09e6221819a")
